20c99808b0d938c71b56d510e2b55b9e3d63e355
alexter42 <lexie@protonmail.ch>
2017-12-29 23:17:45 -0600

    Add string convertion

--- src/imports/api/resolvers/helpers/index.js
+++ src/imports/api/resolvers/helpers/index.js
@@ -2,24 +2,29 @@ import set from 'lodash/set';
 import merge from 'lodash/merge';
 import find from 'lodash/find';
 
-export default function jsonifyXlsx(sheet) {
-  const filterSheet = function() {
-    let header = {};
-    let content = {};
-    Object.entries(sheet).filter(value => {
-      let row = value[0].match(/[0-9]+/g) ? parseInt(value[0].match(/[0-9]+/g)[0]) : null;
-      row >= 7 ? (content[value[0]] = value[1]) : null;
-      row === 3 ? (header[value[0].match(/[A-Z]+/g)] = value[1]) : null;
-    });
+const filterSheet = function(s) {
+  let header = {};
+  let content = {};
+  let type = {};
+  Object.entries(s).filter(value => {
+    let row = value[0].match(/[0-9]+/g) ? parseInt(value[0].match(/[0-9]+/g)[0]) : null;
+    row >= 7 ? (content[value[0]] = value[1]) : null;
+    row === 3 ? (header[value[0].match(/[A-Z]+/g)] = value[1]) : null;
+    row === 2 ? (type[value[0].match(/[A-Z]+/g)] = value[1]) : null;
+    return value;
+  });
 
-    let filteredSheet = {
-      header: header,
-      content: content,
-    };
-    return filteredSheet;
+  let filteredSheet = {
+    header: header,
+    content: content,
+    types: type,
   };
+  return filteredSheet;
+};
 
-  let data = filterSheet();
+export default function jsonifyXlsx(sheet) {
+  const toType = function() {};
+  let data = filterSheet(sheet);
 
   let columns = Object.entries(data.header).map(d => d[0].match(/[A-Z]+/g)[0]);
 
@@ -32,7 +37,12 @@ export default function jsonifyXlsx(sheet) {
 
   Object.keys(rows).map(row => {
     let currentRow = {};
+
     columns.map(column => {
+      data.content[column + row] && data.types[column].v === 'string'
+        ? (data.content[column + row].v = String(data.content[column + row].v))
+        : null;
+
       data.content[column + row]
         ? (currentRow[column] = {
             path: data.header[column].v,
