8f10d4431fa0dde75dd957ff7bc41b603c219eef
alexter42 <lexie@protonmail.ch>
2018-03-03 19:56:01 -0600

    Append shareholders

--- src/imports/api/resolvers/mutations/upload0.js
+++ src/imports/api/resolvers/mutations/upload0.js
@@ -3,7 +3,10 @@ import merge from 'lodash/merge';
 import map from 'lodash/map';
 import assign from 'lodash/assign';
 import find from 'lodash/find';
+
+import groupBy from 'lodash/groupBy';
 import helper from '../helpers';
+import acc from '../helpers/acc';
 
 import File0 from '../../collections/file0';
 
@@ -32,16 +35,35 @@ export default function upload0(root, { file }) {
   allSheets.parties = parties.filter(n => n != undefined);
 
   let accionistas = helper(workbook.Sheets.Accionistas).parties; // Aqui se une a los accionistas (solo en caso de existir)
+
   if (accionistas !== undefined) {
     accionistas = accionistas.filter(n => n != undefined);
-    let mergedParties = map(allSheets.parties, obj =>
-      assign(
-        obj,
-        find(accionistas, {
-          id: obj.id,
-        })
-      )
-    );
+
+    accionistas = groupBy(accionistas, acc => acc.id);
+
+    let shareholders = {};
+
+    Object.entries(accionistas).forEach(([key, value]) => {
+      let sh = [];
+      accionistas[key].filter(value => {
+        sh.push(value.shareholders[0]);
+        return value.shareholders[0];
+      });
+      shareholders[key] = {
+        shareholders: sh,
+      };
+    });
+
+    // console.log(allSheets.parties);
+
+    let mergedParties = map(allSheets.parties, obj => {
+      if (shareholders[obj.id]) {
+        obj.shareholders = shareholders[obj.id].shareholders;
+      }
+      return obj;
+    });
+    console.log(mergedParties);
+
     allSheets.parties = mergedParties;
   }
 
