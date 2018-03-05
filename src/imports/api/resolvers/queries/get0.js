0948b5c595c3f75e638cf91f78b07821ea16cbc7
alexter42 <lexie@protonmail.ch>
2018-03-04 18:10:15 -0600

    Add extension for validation

--- src/imports/api/resolvers/queries/get0.js
+++ src/imports/api/resolvers/queries/get0.js
@@ -6,9 +6,12 @@ export default function() {
   let d = new Date();
   const n = d.toISOString();
   let copy = Object.assign({}, ...file);
-  copy.date = n;
+  copy.date = '2015-09-30T12:09:30-06:00';
   copy.initiationType = 'ppp';
   copy.tag = ['planning'];
+  copy.language = 'es';
+  copy.title = 'Desaladora de Sonora';
+  copy.description = 'Desaladora de Sonora';
 
   let template = {
     uri:
@@ -16,6 +19,17 @@ export default function() {
     version: '1.1',
     extensions: [
       'https://raw.githubusercontent.com/open-contracting/ocds-shareholders-extension/master/extension.json',
+      'https://raw.githubusercontent.com/open-contracting/ocds_requirements_extension/master/extension.json',
+      'https://raw.githubusercontent.com/open-contracting/ocds_milestone_documents_extension/v1.1.1/extension.json',
+      'https://raw.githubusercontent.com/open-contracting/ocds_process_title_extension/v1.1.1/extension.json',
+      'https://raw.githubusercontent.com/open-contracting/ocds_bid_extension/v1.1.1/extension.json',
+      'https://raw.githubusercontent.com/open-contracting/ocds_lots_extension/v1.1.1/extension.json',
+      'https://raw.githubusercontent.com/open-contracting/ocds_documentation_extension/master/extension.json',
+      'https://raw.githubusercontent.com/open-contracting/ocds_budget_breakdown_extension/master/extension.json',
+      'https://raw.githubusercontent.com/open-contracting/ocds_metrics_extension/master/extension.json',
+      'https://raw.githubusercontent.com/open-contracting/ocds_budget_projects_extension/master/extension.json',
+      'https://raw.githubusercontent.com/open-contracting/ocds_location_extension/v1.1.1/extension.json',
+      'https://raw.githubusercontent.com/open-contracting/public-private-partnerships/master/extension.json',
     ],
     publishedDate: '2014-02-02T13:02:00Z',
     license: 'http://opendatacommons.org/licenses/pddl/1.0/',
@@ -26,6 +40,7 @@ export default function() {
       uid: '0000',
       uri: 'https://www.transparency.org/whoweare/contact/org/nc_mexico',
     },
+    ocid: 'ocds-tme990513-01-0000',
     releases: [copy],
   };
 
